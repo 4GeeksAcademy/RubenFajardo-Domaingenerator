c3fd5d0f4eaa8ba387b700dec8ceaf77b4eee2ec
RubFajardo <rubenfajardo68@gmail.com>
2025-05-14 11:44:33 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let ext = ['.com', '.net', '.io', '.es']
+
+let domain = []
+
+for (let pron of pronoun) {
+  domain[0] = pron
+  for (let ad of adj) {
+    domain[1] = ad
+    for (let n of noun) {
+      domain[2] = n
+      for (let e of ext){
+        domain[3] = e
+        console.log(domain.join(''))
+      }
+    }
+  }
+}
